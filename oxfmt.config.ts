ef77592264281face70392c8c351b5487f0445e0
ro80t <ro80ttt@gmail.com>
2026-09-22 17:45:35 +0900

    first commit

---
+++ oxfmt.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "oxfmt";
+
+export default defineConfig({
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  endOfLine: "lf",
+  ignorePatterns: [],
+});
